cf4dd6cd1c6c5073d4293be463a3f693a5b140f6
TDEBORD1 <TDEBORD1@ford.com>
2024-03-27 14:16:33 -0400

    first commit

---
+++ ButtonCharSpacingIssue/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ButtonCharSpacingIssue;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ ButtonCharSpacingIssue/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ButtonCharSpacingIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
