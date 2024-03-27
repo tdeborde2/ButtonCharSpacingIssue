abae6b612556e31f97a13a9bfe90364dafc361a2
tdeborde2 <tdeborde2@gmail.com>
2024-03-27 14:43:24 -0400

    Issue commit

--- ButtonCharSpacingIssue/MainPage.xaml.cs
+++ ButtonCharSpacingIssue/MainPage.xaml.cs
@@ -1,25 +1,67 @@
-﻿namespace ButtonCharSpacingIssue;
+﻿using Microsoft.Maui.Platform;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ButtonCharSpacingIssue;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	int count, count1, count2 = 0;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
+		var button = (Button)sender;
 		count++;
 
 		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+			button.Text = $"Clicked {count} time";
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+			button.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		SemanticScreenReader.Announce(button.Text);
 	}
+
+    private void OnCounterClickNotWorking(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        count1++;
+
+        if (count == 1)
+            button.Text = $"Clicked {count1} time";
+        else
+            button.Text = $"Clicked {count1} times";
+
+        SemanticScreenReader.Announce(button.Text);
+    }
+
+    private void OnCounterClickedWorkAround(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        count2++;
+
+        if (count == 1)
+            button.Text = $"Clicked {count2} time";
+        else
+            button.Text = $"Clicked {count2} times";
+
+        SemanticScreenReader.Announce(button.Text);
+
+#if IOS || MACCATALYST
+        var platformButton = button.Handler?.PlatformView as UIKit.UIButton;
+        if (platformButton != null)
+        {
+            var titleLabel = platformButton.TitleLabel;
+            titleLabel.Text = button.Text;
+
+            var attributedText = titleLabel.AttributedText?.WithCharacterSpacing(button.CharacterSpacing);
+            platformButton.SetAttributedTitle(attributedText, UIKit.UIControlState.Normal);
+        }
+#endif
+    }
 }
 
 
